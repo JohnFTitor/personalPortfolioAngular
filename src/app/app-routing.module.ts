b6b7f2a897b4ba3316aad3e0382be2f33e668715
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2023-10-01 21:36:10 -0500

    Define base routing and intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: '', component: IntroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
