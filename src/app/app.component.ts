d355a198f1bb04f1b2e77601d5dbc49b31108da4
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2024-12-15 15:49:01 -0500

    Improve animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
+import { style, transition, animate, trigger} from '@angular/animations';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)'}),
+        animate('150ms ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('150ms ease-in', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   isSidePanelOpen = signal(false);
--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,4 +1,3 @@
-import { style, transition, animate, trigger} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,17 +6,6 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.scss',
-  animations: [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('150ms ease-out', style({ transform: 'translateX(0)' }))
-      ]),
-      transition(':leave', [
-        animate('150ms ease-in', style({ transform: 'translateX(-100%)' }))
-      ])
-    ])
-  ]
 })
 export class SidePanelComponent {
 
