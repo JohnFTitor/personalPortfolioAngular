4ee20c1ee7b61935a4077af6a1d5314306c43d4c
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2025-01-04 17:05:08 -0500

    Move animation inside component

--- src/app/learning-element/learning-element.component.ts
+++ src/app/learning-element/learning-element.component.ts
@@ -1,5 +1,4 @@
-import { Component, input, signal } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-learning-element',
@@ -11,4 +10,27 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 export class LearningElementComponent {
   item = input<string>();
   index = input<number>(0);
+
+  ngAfterViewInit() {
+    // Get all slide elements
+    setTimeout(() => {
+      const slide = document.querySelector(`#slide-${this.index()}`);
+
+      if (!slide) return;
+
+      // Calculate the ratio of element width to parent width
+      const parentWidth =
+        slide.parentElement?.getBoundingClientRect().width || 0;
+      const elementWidth = slide.getBoundingClientRect().width;
+      const ratio = elementWidth / parentWidth;
+
+      // Set the CSS variable for this specific element
+      slide.setAttribute(
+        'style',
+        `--element-width-ratio: ${ratio.toString()}; ${slide.getAttribute(
+          'style'
+        )}`
+      );
+    }, 0);
+  }
 }
